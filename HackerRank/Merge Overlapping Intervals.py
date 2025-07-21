f30a47986c0125bb4d02576880c3c243a78e74fb
Rishij Manna <rishijmanna2018@gmail.com>
2025-07-21 10:18:39 +0530

    Delete HackerRank directory

--- HackerRank/Merge Overlapping Intervals.py
+++
@@ -1,3 +0,0 @@
-# Question Link-> https://www.hackerrank.com/contests/array-contest-2/challenges/merge-overlapping-intervals-5
-
-print("hi")
--- HackerRank/Swap_Case.py
+++
@@ -1,6 +0,0 @@
-#Question link : https://www.hackerrank.com/challenges/swap-case
-
-def swap_case(s):
-    return s.swapcase()
-s= input()
-print(swap_case(s))
