435a78edd4920e104b12638206f78af88d290076
Rishij Manna <rishijmanna2018@gmail.com>
2025-07-22 20:54:11 +0530

    Create Sort the Unsorted.py

---
+++ python/Daily Contest/Sort the Unsorted.py
@@ -0,0 +1,6 @@
+#Link -> https://www.hackerrank.com/contests/classroomtech-coding-contest/challenges/sort-the-unsorted/submissions/code/1394694421
+
+n=int(input())
+l = list(map(int,input().split()))
+l.sort()
+print(*l)
