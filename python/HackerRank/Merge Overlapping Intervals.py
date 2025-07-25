20c72da2a340c031cbc3919168c930ba1f1efdd8
Rishij Manna <rishijmanna2018@gmail.com>
2025-07-25 23:32:39 +0530

    Update Merge Overlapping Intervals.py

--- python/HackerRank/Merge Overlapping Intervals.py
+++ python/HackerRank/Merge Overlapping Intervals.py
@@ -1,3 +1,14 @@
 # Question Link-> https://www.hackerrank.com/contests/array-contest-2/challenges/merge-overlapping-intervals-5
 
-print("hi")
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort()
+        merge=[]
+        for i in intervals:
+           
+            if not merge or merge[-1][1]<i[0]:
+                merge.append(i)
+            elif merge[-1][1]>= i[0]:
+                merge[-1][1]=max(merge[-1][1],i[1])
+        return merge
+        
