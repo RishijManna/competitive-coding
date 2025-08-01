282812dee51c3677b23005bb44a18be704edc706
Rishij Manna <rishijmanna2018@gmail.com>
2025-08-02 00:01:25 +0530

    Update rabbits-in-forest.py

--- python/Leetcode/rabbits-in-forest.py
+++ python/Leetcode/rabbits-in-forest.py
@@ -1,15 +1,17 @@
-def numRab(ans):
-    f={}
-    t=0
-    for i in ans:
-        if i==0:
-            t+=1  
-        elif i not in f:
-            f[i]=1 
-            t+=i+1  
-        else:
-            f[i]+=1
-            if f[i]>i+1:
-                f[i]=1
-                t+=i+1
-    return t
+# https://leetcode.com/problems/rabbits-in-forest/submissions/1719797445/
+class Solution(object):
+    def numRabbits(self, ans):
+        f={}
+        t=0
+        for i in ans:
+            if i==0:
+                t+=1  
+            elif i not in f:
+                f[i]=1 
+                t+=i+1  
+            else:
+                f[i]+=1
+                if f[i]>i+1:
+                    f[i]=1
+                    t+=i+1
+        return t
