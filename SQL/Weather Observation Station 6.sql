a76ea697ad8ef1fc24e57818833e0d58475a7072
Rishij Manna <rishijmanna2018@gmail.com>
2025-08-10 12:25:23 +0530

    Create Weather Observation Station 6.sql

---
+++ SQL/Weather Observation Station 6.sql
@@ -0,0 +1,3 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+
+select distinct(city) from station where city like 'A%' or  city like 'E%' or city like 'I%'  or city like 'O%'  or city like 'U%';
