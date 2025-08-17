64bc4077a75cce2cba266f368d0b5c7ec94c0b4f
Rishij Manna <rishijmanna2018@gmail.com>
2025-08-17 11:55:13 +0530

    Create Spiral Matrix1.py

---
+++ python/Leetcode/Spiral Matrix1.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        l,t=0,0
+        m=len(matrix)
+        n=len(matrix[0])
+        r=n-1
+        b=m-1
+        ans=[]  
+        while l<=r and t<=b:
+            for i in range(l,r+1):
+                ans.append(matrix[t][i])
+            t+=1
+            for i in range(t,b+1):
+                ans.append(matrix[i][r])
+            r-=1
+
+            if t<=b:
+                for i in range(r,l-1,-1):
+                    
+                    ans.append(matrix[b][i])
+            b-=1
+
+            if l<=r:
+                for i in range(b,t-1,-1):
+                    ans.append(matrix[i][l])
+            l+=1
+        return ans
